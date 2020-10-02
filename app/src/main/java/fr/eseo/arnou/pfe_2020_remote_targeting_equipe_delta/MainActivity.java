e40d94b03edf7945e54474d03affb044cc8c969d
Ogrys <adelin.a@outlook.fr>
2020-10-02 13:59:30 +0200

    etape 1 connexion

--- app/src/main/java/fr/eseo/arnou/pfe_2020_remote_targeting_equipe_delta/MainActivity.java
+++ app/src/main/java/fr/eseo/arnou/pfe_2020_remote_targeting_equipe_delta/MainActivity.java
@@ -2,14 +2,40 @@ package fr.eseo.arnou.pfe_2020_remote_targeting_equipe_delta;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnOnOff, btnDiscover, btnSend;
+    ListView listView;
+    TextView read_msg_box, connectionStatus;
+    EditText writeMsg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //test
+        initialWork();
+
+
+    }
+
+    private void initialWork() {
+        btnOnOff = (Button) findViewById(R.id.onOff);
+        btnDiscover = (Button) findViewById(R.id.discover);
+        btnSend = (Button) findViewById(R.id.sendButton);
+        listView = (ListView) findViewById(R.id.peerListView);
+        read_msg_box = (TextView) findViewById(R.id.readMsg);
+        connectionStatus = (TextView) findViewById(R.id.connectionStatus);
+        writeMsg = (EditText) findViewById(R.id.writeMsg);
     }
 }
