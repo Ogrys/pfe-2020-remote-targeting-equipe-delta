49f526ff69da543c242e61069b240d30219835e7
Ogrys <adelin.a@outlook.fr>
2020-10-02 14:36:11 +0200

    fonction de la classe nouvelle

--- app/src/main/java/fr/eseo/arnou/pfe_2020_remote_targeting_equipe_delta/Network/WifiDirectBroadcastReceiver.java
+++ app/src/main/java/fr/eseo/arnou/pfe_2020_remote_targeting_equipe_delta/Network/WifiDirectBroadcastReceiver.java
@@ -3,11 +3,33 @@ package fr.eseo.arnou.pfe_2020_remote_targeting_equipe_delta.Network;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.wifi.p2p.WifiP2pManager;
+
+import fr.eseo.arnou.pfe_2020_remote_targeting_equipe_delta.MainActivity;
 
 public class WifiDirectBroadcastReceiver extends BroadcastReceiver {
+    private WifiP2pManager mManager;
+    private WifiP2pManager.Channel mChannel;
+    private MainActivity mActivity;
+
+    public WifiDirectBroadcastReceiver(WifiP2pManager mManager, WifiP2pManager.Channel mChannel, MainActivity mActivity){
+        this.mManager = mManager;
+        this.mChannel = mChannel;
+        this.mActivity = mActivity;
+    }
 
     @Override
     public void onReceive(Context context, Intent intent){
+        String action = intent.getAction();
 
+        if(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)){
+            //do something
+        }else if(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)){
+            //do something
+        }else if(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)){
+            //do something
+        }else if(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)){
+            //do something
+        }
     }
 }
