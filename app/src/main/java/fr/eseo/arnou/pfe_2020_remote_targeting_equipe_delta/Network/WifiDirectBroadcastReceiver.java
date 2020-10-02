e98dd6568145fe336c865ccb0b9c7c3816f05ac9
Ogrys <adelin.a@outlook.fr>
2020-10-02 15:54:27 +0200

    commit

--- app/src/main/java/fr/eseo/arnou/pfe_2020_remote_targeting_equipe_delta/Network/WifiDirectBroadcastReceiver.java
+++ app/src/main/java/fr/eseo/arnou/pfe_2020_remote_targeting_equipe_delta/Network/WifiDirectBroadcastReceiver.java
@@ -37,7 +37,7 @@ public class WifiDirectBroadcastReceiver extends BroadcastReceiver {
             }
         } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
             if (mManager != null) {
-                if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                     // TODO: Consider calling
                     //    ActivityCompat#requestPermissions
                     // here to request the missing permissions, and then overriding
