60d6c0dad5c7671c9c8bf43323b029e85ef302a5
Ogrys <adelin.a@outlook.fr>
2020-10-02 10:12:01 +0200

    Initial commit

---
+++ app/src/main/java/fr/eseo/arnou/pfe_2020_remote_targeting_equipe_delta/MainActivity.java
@@ -0,0 +1,14 @@
+package fr.eseo.arnou.pfe_2020_remote_targeting_equipe_delta;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
