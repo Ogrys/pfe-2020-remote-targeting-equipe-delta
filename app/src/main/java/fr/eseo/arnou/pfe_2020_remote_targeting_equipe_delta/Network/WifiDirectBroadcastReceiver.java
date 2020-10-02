bed532a253573030784508cadc459c96a1a873ec
Ogrys <adelin.a@outlook.fr>
2020-10-02 14:42:57 +0200

    suite des fontions

--- app/src/main/java/fr/eseo/arnou/pfe_2020_remote_targeting_equipe_delta/Network/WifiDirectBroadcastReceiver.java
+++ app/src/main/java/fr/eseo/arnou/pfe_2020_remote_targeting_equipe_delta/Network/WifiDirectBroadcastReceiver.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.net.wifi.p2p.WifiP2pManager;
+import android.widget.Toast;
 
 import fr.eseo.arnou.pfe_2020_remote_targeting_equipe_delta.MainActivity;
 
@@ -23,7 +24,13 @@ public class WifiDirectBroadcastReceiver extends BroadcastReceiver {
         String action = intent.getAction();
 
         if(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)){
-            //do something
+            int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE,-1);
+
+            if(state==WifiP2pManager.WIFI_P2P_STATE_ENABLED){
+                Toast.makeText(context, "Wifi is ON",Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(context, "Wifi is OFF",Toast.LENGTH_SHORT).show();
+            }
         }else if(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)){
             //do something
         }else if(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)){
